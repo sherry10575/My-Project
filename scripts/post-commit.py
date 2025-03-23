876577a0448ae25b178c5df791b857f278999109
sherry10575 <sherry10@uni.edu>
2025-03-24 00:08:23 +0500

    "Test commit"

--- scripts/post-commit.py
+++ scripts/post-commit.py
@@ -2,7 +2,7 @@ import os
 import pylint.lint
 
 repo_path = os.path.dirname(os.path.abspath(__file__))
-checks = ["C0301", "C0302", "C0303", "E0602", "W0611"]
+checks = ["C0301", "C0303", "E0602", "W0611", "R0913"]
 
 files = [f for f in os.listdir(repo_path) if f.endswith(".py")]
 
